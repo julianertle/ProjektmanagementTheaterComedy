90cc8d23217ce9ba603868bca75ba1e801e7e330
Julianertle <julian_ertle@web.de>
2022-12-08 20:32:32 +0100

    Extending module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,4 +7,11 @@ module de.hhn.ai.pmt.theatercomedy {
 
     opens de.hhn.ai.pmt.theatercomedy to javafx.fxml;
     exports de.hhn.ai.pmt.theatercomedy;
+
+    opens de.hhn.ai.pmt.theatercomedy.model to javafx.fxml;
+    exports de.hhn.ai.pmt.theatercomedy.model;
+    exports de.hhn.ai.pmt.theatercomedy.control;
+    opens de.hhn.ai.pmt.theatercomedy.control to javafx.fxml;
+    exports de.hhn.ai.pmt.theatercomedy.view;
+    opens de.hhn.ai.pmt.theatercomedy.view to javafx.fxml;
 }
