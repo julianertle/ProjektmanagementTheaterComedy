e26ede8248a36f61a366b3dd63c10e864757d6dd
Julianertle <julian_ertle@web.de>
2022-12-09 18:30:21 +0100

    Added more menu options

--- src/main/java/de/hhn/ai/pmt/theatercomedy/control/Controller.java
+++ src/main/java/de/hhn/ai/pmt/theatercomedy/control/Controller.java
@@ -2,18 +2,13 @@ package de.hhn.ai.pmt.theatercomedy.control;
 
 import de.hhn.ai.pmt.theatercomedy.view.RegistrierenView;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
-import java.net.URL;
+public class Controller {
 
-public class StartseiteController {
-
+    @FXML
+    public Button homeButton;
     @FXML
     private Button favoriteButton;
     @FXML
