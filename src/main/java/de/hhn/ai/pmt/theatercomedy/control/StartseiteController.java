314c7baa031b75f2ce9612f727bb1e94ce39de94
Julianertle <julian_ertle@web.de>
2022-12-09 18:06:30 +0100

    Connecting registrieren-view.fxml with Favoriten button for test purpose

--- src/main/java/de/hhn/ai/pmt/theatercomedy/control/StartseiteController.java
+++ src/main/java/de/hhn/ai/pmt/theatercomedy/control/StartseiteController.java
@@ -1,11 +1,13 @@
 package de.hhn.ai.pmt.theatercomedy.control;
 
+import de.hhn.ai.pmt.theatercomedy.view.RegistrierenView;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.net.URL;
@@ -18,18 +20,17 @@ public class StartseiteController {
     protected void onFavoritenButtonClick() {
         System.out.println("----- Favoriten Button geklickt -----");
 
-            try {
-                ClassLoader classLoader = getClass().getClassLoader();
-                URL fxmlUrl = classLoader.getResource("de/hhn/ai/pmt/theatercomedy/hello-view.fxml");
-                FXMLLoader fxmlLoader = new FXMLLoader();
-                fxmlLoader.setLocation(fxmlUrl);
-                Node root = fxmlLoader.load();
+        RegistrierenView view = new RegistrierenView();
+        view.startView(favoriteButton);
+    }
 
-                // get the current scene and set its root to the new node
-                Scene scene = favoriteButton.getScene();
-                scene.setRoot((Parent) root);
-            } catch (Exception e) {
-                e.printStackTrace(System.out);
-            }
+
+    // for registration
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onRegistrationButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
